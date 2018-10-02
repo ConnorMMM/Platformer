a234ba7b590ee775b3146a3d5653c3c989992628
ConnorMMM <connorchurchmill@gmail.com>
2018-10-02 14:57:19 +1000

    Added map
    Added code for the map to be generated and the files needed to do so

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Graphics;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace Platformer
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         Player player = new Player(); //Create an instance of our player class
+
+        Camera2D camera = null; //Creates an instance of a 2D camera
+        TiledMap map = null; //Creates an instance of a Tiled map
+        TiledMapRenderer mapRenderer = null; //Creates an instance of what makes a Tiled map
 
         public Game1()
         {
@@ -35,7 +41,16 @@ namespace Platformer
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             player.Load(Content); //Call the 'Load' function in the Player class
-            
+
+            BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, graphics.GraphicsDevice.Viewport.Width, graphics.GraphicsDevice.Viewport.Height);
+
+            camera = new Camera2D(viewportAdapter);
+            camera.Position = new Vector2(0, graphics.GraphicsDevice.Viewport.Height);
+
+            map = Content.Load<TiledMap>("Level1");
+            mapRenderer = new TiledMapRenderer(GraphicsDevice);
+
+
         }
 
         
@@ -56,16 +71,20 @@ namespace Platformer
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Draw(GameTime gameTime)
         {
             //Clears anything previously drawn to the screen
             GraphicsDevice.Clear(Color.Gray);
+
+            var viewMatrix = camera.GetViewMatrix();
+            var projectionMatrix = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0.0f, -1.0f);
+
             //Begin drawing
-            spriteBatch.Begin();
+            spriteBatch.Begin(transformMatrix: viewMatrix);
+
+
+            mapRenderer.Draw(map, ref viewMatrix, ref projectionMatrix);
             // Call the 'Draw' function from our Player class
             player.Draw(spriteBatch);
             //Finish drawing
